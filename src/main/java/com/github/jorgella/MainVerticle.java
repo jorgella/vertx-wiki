4d46974049d7821a5b26dcae9f939f214bd395bd
Jorge Luis L. Amorim <lopesluisjorge@hotmail.com>
2022-06-16 21:29:53 -0300

    Initial commit

---
+++ src/main/java/com/github/jorgella/MainVerticle.java
@@ -0,0 +1,12 @@
+package com.github.jorgella;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
